d5bfaaf4fd28eb041086774bb59cf44ead241d94
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-11-17 17:35:36 -0600

    Add [0] to the end of lines 1 and 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finsihed.
 
-var section = document.getElementsByTagName('section');
-var button = document.getElementsByTagName('button');
+var section = document.getElementsByTagName('section')[0];
+var button = document.getElementsByTagName('button')[0];
 
 function slideToggle() {
   section.classList.toggle('hide');
