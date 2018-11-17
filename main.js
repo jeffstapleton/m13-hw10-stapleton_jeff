2b32ddd2051cbbf713410c2cbfd2e90b3462dfc0
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-11-17 17:26:49 -0600

    Create button varible

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finsihed.
 
 var section = document.getElementsByTagName('section');
+var button = document.getElementsByTagName('button');
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
