ddbbe93d051d1a07efc249546aec646d14c2e0ae
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-11-17 17:47:37 -0600

    Finish assignment

--- main.js
+++ main.js
@@ -11,5 +11,7 @@ button.addEventListener('click', slideToggle);
 */
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
 $(document).ready(function(){
-  
+  $('button').click(function (){
+    $('section').slideToggle();
+  });
 });
