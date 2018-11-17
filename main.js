5ecf171e58ca8f2c50e5fa330adaf8ebcf5be9bb
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-11-17 17:36:59 -0600

    Comment out vanilla JS code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finsihed.
-
+/* 
 var section = document.getElementsByTagName('section')[0];
 var button = document.getElementsByTagName('button')[0];
 
@@ -8,5 +8,5 @@ function slideToggle() {
 }
 
 button.addEventListener('click', slideToggle);
-
+*/
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
