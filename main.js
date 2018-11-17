77f1a07e7e01f13cfdaa7e16dea24a6a04a10859
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-11-17 17:29:40 -0600

    Add slideToggle function

--- main.js
+++ main.js
@@ -3,5 +3,9 @@
 var section = document.getElementsByTagName('section');
 var button = document.getElementsByTagName('button');
 
+function slideToggle() {
+  section.classList.toggle('hide')
+}
+
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
